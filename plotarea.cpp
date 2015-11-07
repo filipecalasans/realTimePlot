f7e4237a7d5427732e67c518651faf2cc6b55bdc
Filipe Calasans <filipe.calasans@gmail.com>
2015-11-07 02:34:59 -0200

    plot update implemented

--- plotarea.cpp
+++ plotarea.cpp
@@ -1,6 +1,8 @@
 #include "plotarea.h"
 #include "ui_plotarea.h"
 
+#include <qmath.h>
+
 PlotArea::PlotArea(QWidget *parent) :
     QCustomPlot(parent),
     ui(new Ui::PlotArea)
@@ -15,12 +17,71 @@ PlotArea::PlotArea(QWidget *parent) :
     wideAxisRect->removeAxis(wideAxisRect->axis(QCPAxis::atRight));
     wideAxisRect->removeAxis(wideAxisRect->axis(QCPAxis::atTop));
     plotLayout()->addElement(0, 0, wideAxisRect);
+
+    startTimer(PERIOD_OF_FRAMES);
 }
 
 PlotArea::~PlotArea()
 {
     delete ui;
 }
+
+void PlotArea::addPointStream(PointStream *points)
+{
+    QCPGraph *graph = addGraph(axisRect(0)->axis(QCPAxis::atBottom), axisRect(0)->axis(QCPAxis::atLeft));
+
+    /*
+     * Adiciona uma Fonte de pontos e uma nova curva associada
+     * a essa fonte de pontos na área de plotagem.
+     *
+     * Add a new point stream and a graph that is associated
+     * to this pointStream to the Plot Area.
+     */
+    pointStream.insert(points, graph);
+}
+
+double PlotArea::getWindowLengthInSeconds() const
+{
+    return windowLengthInSeconds;
+}
+
+void PlotArea::setWindowLengthInSeconds(double value)
+{
+    windowLengthInSeconds = value;
+}
+
+void PlotArea::timerEvent(QTimerEvent *event)
+{
+    Q_UNUSED(event)
+    update();
+}
+
+void PlotArea::update()
+{
+    for(PointStream *stream : pointStream.keys()) {
+        QList<QPointF> data = stream->getRecentPoints(stream->getSamplesPerSeconds() * windowLengthInSeconds);
+        QCPDataMap *dataMap = new QCPDataMap();
+        quint32 windowNumberLastSample = ((quint32)data.last().x())%((quint32)(windowLengthInSeconds*stream->getSamplesPerSeconds()));
+        for(int i=data.size()-1; i>=0; i++) {
+            QPointF point = data[i];
+            quint32 windowNumber = ((quint32)point.x())%((quint32)(windowLengthInSeconds*stream->getSamplesPerSeconds()));
+            if(windowNumber != windowNumberLastSample) {
+                break;
+            }
+            double x = ((double)stream->getSamplesPerSeconds())/point.x();
+            dataMap->insert(x, QCPData(x,point.y()));
+        }
+        pointStream[stream]->setData(dataMap, false);
+    }
+
+    axisRect()->axis(QCPAxis::atLeft)->rescale(true);
+
+    replot();
+}
+
+
+
+
 
 
 
--- plotarea.h
+++ plotarea.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 
 #include "QCustomPlot/qcustomplot.h"
+#include "pointstream.h"
 
 namespace Ui {
 class PlotArea;
@@ -17,8 +18,22 @@ public:
     explicit PlotArea(QWidget *parent = 0);
     ~PlotArea();
 
+    void addPointStream (PointStream *points);
+    void update();
+
+    double getWindowLengthInSeconds() const;
+    void setWindowLengthInSeconds(double value);
+
+    void timerEvent(QTimerEvent *event);
+
 private:
+
+    static const int PERIOD_OF_FRAMES = 100;
     Ui::PlotArea *ui;
+    QMap<PointStream *, QCPGraph *> pointStream;
+    double windowLengthInSeconds = 4.0;
+
+
 
 };
 
