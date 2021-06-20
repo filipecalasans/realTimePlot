ff65d76f8bb5a50480e0c18a17ac6723bee3884a
Filipe Calasans <filipecalasans@Filipes-MBP.home>
2021-06-20 12:19:52 -0700

    [build][mac] Add a build script for macOS

---
+++ build-macOs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+mkdir "$SCRIPT_DIR/out" &> /dev/null || true
+
+QT5_DIR=$(brew --prefix qt5)
+
+(cd "$SCRIPT_DIR/out" && cmake ../ -DCMAKE_PREFIX_PATH=$QT5_DIR/ && make)
+
