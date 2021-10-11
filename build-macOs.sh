6b4f88bab37b0f0bb1ad396fd4e106c7954abac0
Filipe Calasans <filipe.calasans@gmail.com>
2021-10-10 19:02:17 -0700

    build debug binaries by default on macOS

--- build-macOs.sh
+++ build-macOs.sh
@@ -6,5 +6,5 @@ mkdir "$SCRIPT_DIR/out" &> /dev/null || true
 
 QT5_DIR=$(brew --prefix qt5)
 
-(cd "$SCRIPT_DIR/out" && cmake ../ -DCMAKE_PREFIX_PATH=$QT5_DIR/ && make)
+(cd "$SCRIPT_DIR/out" && cmake ../ -DCMAKE_PREFIX_PATH=$QT5_DIR/ -DCMAKE_BUILD_TYPE=Debug && make)
 
